26627345f273ec16da17331472c2be82b456fa30
Cookiezby <zhubingyi1992@gmail.com>
2018-08-11 16:32:46 +0900

    init

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iOSDC2018' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for iOSDC2018
+
+  target 'iOSDC2018Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ iOSDC2018Tests/iOSDC2018Tests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSDC2018Tests.swift
+//  iOSDC2018Tests
+//
+//  Created by cookie on 2018/8/11.
+//  Copyright © 2018 zhubingyi. All rights reserved.
+//
+
+import XCTest
+@testable import iOSDC2018
+
+class iOSDC2018Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
