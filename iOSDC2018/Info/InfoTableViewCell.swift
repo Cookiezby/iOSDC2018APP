a4a4afbb6cbb3c167daa0e735eaafca4a8d9537b
Cookiezby <zhubingyi1992@gmail.com>
2018-08-19 00:00:36 +0900

    add support for fcm

--- iOSDC2018/Info/InfoTableViewCell.swift
+++ iOSDC2018/Info/InfoTableViewCell.swift
@@ -9,4 +9,25 @@
 import Foundation
 import UIKit
 
+final class InfoTableViewCell: UITableViewCell {
+    
+    private let iconView: UIImageView = {
+        let view = UIImageView()
+        return view
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 
